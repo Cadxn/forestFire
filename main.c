6666e43dff8ba37f602f19158a69360b863eb71e
Cadxn <cadenhurley@gmail.com>
2026-09-11 15:39:07 -0400

    Array and Vars Created

--- main.c
+++ main.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int main(){
-    printf("Hello World");
-    printf("This is my first commit");
+    //srand(time(NULL));
+    /*
+    *int numbers[2][2] = {
+        {0}, {0}
+    };
+    */
+    int rows, cols, seed, probability; 
+
+    printf("rows: ", &rows);
+    scanf("%d", &rows);
+    printf("cols: ", &cols);
+    scanf("%d", &cols);
+
+    int **numbers = (int **)malloc(rows * sizeof(int *));
+    for(int i = 0; i < rows; i++){
+        numbers[i] = (int *)malloc(cols * sizeof(int));
+    }
+
+
+    printf("seed: ", &seed);
+    scanf("%d", &seed);
+    if(seed < 0 ) {return 0;}
+    printf("probability: ", &probability);
+    scanf("%d", &probability);
+    if(probability < 0) {return 0;}
+
+    srand(seed);
+
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+            printf("%d", numbers[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
